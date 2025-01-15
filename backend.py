b472327f9d28d64a8a77fe6bd63212af333da07c
Thomas <thomas.s.casco@gmail.com>
2025-01-15 10:33:44 -0300

    Cambios para optimizar.

--- backend.py
+++ backend.py
@@ -43,34 +43,34 @@ def generar_credencial():
     familiares = empleado.iloc[0]["Familiares"]
 
     # Generar el PDF con estilo mejorado
-    pdf = FPDF()
+    pdf = FPDF(format=(105, 148))  # Tamaño personalizado (A6)
     pdf.add_page()
 
     # Fondo de color claro
-    pdf.set_fill_color(240, 248, 255)  # AliceBlue
-    pdf.rect(0, 0, 210, 297, 'F')
+    pdf.set_fill_color(255, 255, 255)  # AliceBlue
+    pdf.rect(0, 0, 105, 148, 'F')
 
     # Agregar logo
-    logo_path = "logo_imsa.png"  # Ruta al logo de la empresa
+    logo_path = "logo2.jpg"  # Ruta al logo de la empresa
     if os.path.exists(logo_path):
-        pdf.image(logo_path, x=10, y=10, w=30)  # Ajustar posición y tamaño del logo
+        pdf.image(logo_path, x=37.5, y=3, w=30)  # Ajustar posición y tamaño del logo
     else:
         print(f"Advertencia: No se encontró el archivo del logo en {logo_path}")
 
     # Título principal
-    pdf.set_font("Arial", style="B", size=16)
+    pdf.set_font("Arial", style="B", size=12)
     pdf.set_text_color(51, 51, 51)  # Gris oscuro
     pdf.cell(0, 10, "IMSA - Credencial para la Pileta", ln=True, align='C')
     pdf.ln(10)
 
     # Subtítulo decorativo
-    pdf.set_font("Arial", size=12)
+    pdf.set_font("Arial", size=10)
     pdf.set_text_color(255, 107, 107)  # Coral
     pdf.cell(0, 10, "Bienvenido al Verano", ln=True, align='C')
-    pdf.ln(20)
+    pdf.ln(10)
 
     # Información del empleado
-    pdf.set_font("Arial", size=12)
+    pdf.set_font("Arial", size=10)
     pdf.set_text_color(0, 0, 0)
     pdf.cell(0, 10, f"Apellido y Nombre: {nombre}", ln=True, align='L')
     pdf.cell(0, 10, f"DNI: {dni}", ln=True, align='L')
@@ -79,13 +79,13 @@ def generar_credencial():
 
     # Mensaje de invitación
     pdf.set_text_color(255, 107, 107)  # Coral
-    pdf.set_font("Arial", style="B", size=12)
+    pdf.set_font("Arial", style="B", size=10)
     pdf.cell(0, 10, "Pileta SPIQYP - Válido hasta el 28/02/25", ln=True, align='C')
 
     # Footer
-    pdf.set_font("Arial", style="I", size=10)
+    pdf.set_font("Arial", style="I", size=8)
     pdf.set_text_color(136, 136, 136)  # Gris claro
-    pdf.ln(20)
+    pdf.ln(10)
     pdf.cell(0, 10, "Equipo de RRHH", ln=True, align='C')
 
     # Guardar el PDF como bytes
@@ -96,6 +96,4 @@ def generar_credencial():
     return send_file(pdf_buffer, as_attachment=True, download_name=f"credencial_{dni}.pdf")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Obtiene el puerto de la variable de entorno o usa 5000 por defecto
-    app.run(host="0.0.0.0", port=port)  # Cambia el host a 0.0.0.0 para que sea accesible externamente
-
+    app.run(debug=True)
